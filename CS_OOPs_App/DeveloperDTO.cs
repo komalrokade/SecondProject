ebd271e55b0ac61da66c573368c411a7d67f7130
Komal Rokade <komal.rokade@simplifyhealthcare.com>
2022-05-22 10:47:01 -0700

    Add project files.

---
+++ CS_OOPs_App/DeveloperDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_OOPs_App
+{
+    class DeveloperDTO : EmployeeDTO
+    {
+        public int ProfitShare { get; set; }
+        public int Alloances { get; set; }
+    }
+}
---
+++ CS_OOPs_App/EngineerDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_OOPs_App
+{
+    class EngineerDTO : EmployeeDTO
+    {
+        public int ProfitShare { get; set; }
+        public int Alloances { get; set; }
+    }
+}
+}
